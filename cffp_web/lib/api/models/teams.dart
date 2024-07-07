8742c10f62c5d0839f6a054622a3aeba41a56d1a
Ethan Chase <ethanchase@cmail.carleton.ca>
2024-07-07 18:29:13 -0400

    Gametimes & Week 1

---
+++ cffp_web/lib/api/models/teams.dart
@@ -0,0 +1,22 @@
+import 'package:cffp_web/api/models/divisions.dart';
+
+final teams = {
+  "ari": {
+    "abbreviation": "ARI",
+    "city": "Arizona",
+    "division": Divisions.nfcWest,
+    "name": "Cardinals",
+  },
+  "atl": {
+    "abbreviation": "ATL",
+    "city": "Atlanta",
+    "division": Divisions.nfcSouth,
+    "name": "Falcons",
+  },
+  "bal": {
+    "abbreviation": "BAL",
+    "city": "Baltimore",
+    "division": Divisions.afcNorth,
+    "name": "Ravens",
+  }
+};
